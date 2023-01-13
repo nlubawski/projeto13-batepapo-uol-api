c0fa8fda2ff40cb7d5abf2eb793672cecad75dc1
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-13 17:54:18 -0300

    fix: update return statusCode in post status

--- src/app.js
+++ src/app.js
@@ -135,7 +135,7 @@ app.post("/status", async (req, res) => {
   const user = req.headers.user
   try {
     const existsUser = await db.collection("participants").find({ name: user }).toArray()
-    if (!existsUser) return res.sendStatus(404)
+    if (!existsUser) return res.sendStatus(200)
     await db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() } })
     return res.statusCode(200)
 
